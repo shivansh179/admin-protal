e490bd93a531f9c5cc587cc8e2511892b700764c
Shivansh Shukla <prasantshukla89@gmail.com>
2024-04-25 18:10:21 +0530

    Fixed Errors

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,13 +1,17 @@
-import { Children } from "react";
+import React from "react";
 import SideNav from "../ui/dashboard/sidenav";
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+export default function Layout({ children }: LayoutProps) {
   return (
-          <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
-              <div className="w-full flex-none md:w-64">
-                <SideNav/>
-              </div>
-              <div className="w-full flex-none md:w-64">{children}</div>
-          </div>
-  )
+    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+      <div className="w-full flex-none md:w-64">
+        <SideNav />
+      </div>
+      <div className="w-full flex-none md:w-64">{children}</div>
+    </div>
+  );
 }
